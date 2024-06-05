2b999d1401f6b9b13ca2827ac889c8b055b38a04
raldblox <raldblox@outlook.com>
2024-06-06 01:36:53 +0800

    added: frontend

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
+import { NextUIProvider } from "@nextui-org/react";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
     </html>
   );
 }
