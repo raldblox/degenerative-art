897e3af5364a9b05b97b578663b41d7e8f3bbb4a
raldblox <raldblox@outlook.com>
2024-06-08 07:45:11 +0800

    handle og fetch error

--- frontend/app/id/[tokenId]/opengraph-image.js
+++ frontend/app/id/[tokenId]/opengraph-image.js
@@ -14,50 +14,72 @@ export const contentType = "image/png";
 
 // Image generation
 export default async function Image({ params }) {
-  const protocol =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://www.degeneratives.art";
-  const url = `${protocol}/api/getTokenURI/${params.tokenId}`;
-  const metadata = await fetch(url).then((res) => res.json());
-  console.log(metadata);
+  try {
+    const protocol =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://www.degeneratives.art";
+    const url = `${protocol}/api/getTokenURI/${params.tokenId}`;
+    const metadata = await fetch(url).then((res) => res.json());
+    console.log(metadata);
 
-  const emoji1 = decodeURIComponent(
-    escape(metadata.tokenURI.attributes[0].value)
-  );
-  const emoji2 = decodeURIComponent(
-    escape(metadata.tokenURI.attributes[1].value)
-  );
-  const emoji3 = decodeURIComponent(
-    escape(metadata.tokenURI.attributes[2].value)
-  );
+    const emoji1 = decodeURIComponent(
+      escape(metadata.tokenURI.attributes[0].value)
+    );
+    const emoji2 = decodeURIComponent(
+      escape(metadata.tokenURI.attributes[1].value)
+    );
+    const emoji3 = decodeURIComponent(
+      escape(metadata.tokenURI.attributes[2].value)
+    );
 
-  return new ImageResponse(
-    (
-      // ImageResponse JSX element
-      <div
-        style={{
-          background: "black",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center", // Distribute rows evenly
-        }}
-      >
-        <div style={{ display: "flex", gap: "70px", fontSize: 300 }}>
-          <span>{emoji1}</span>
-          <span>{emoji2}</span>
-          <span>{emoji3}</span>
+    return new ImageResponse(
+      (
+        // ImageResponse JSX element
+        <div
+          style={{
+            background: "black",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center", // Distribute rows evenly
+          }}
+        >
+          <div style={{ display: "flex", gap: "70px", fontSize: 300 }}>
+            <span>{emoji1}</span>
+            <span>{emoji2}</span>
+            <span>{emoji3}</span>
+          </div>
+          <p style={{ color: "#aaa", fontSize: 24 }}>
+            degeneratives.art #{params.tokenId}
+          </p>
         </div>
-        <p style={{ color: "#aaa", fontSize: 24 }}>
-          degeneratives.art #{params.tokenId}
-        </p>
-      </div>
-    ),
-    {
-      ...size,
-    }
-  );
+      ),
+      {
+        ...size,
+      }
+    );
+  } catch (error) {
+    console.error("Error in Image component:", error);
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            fontSize: 128,
+            background: "white",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          degeneratives.art
+        </div>
+      ),
+      { ...size }
+    );
+  }
 }
