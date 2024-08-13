108ce075b902cdc4318eb4615c9289f57ee82a74
raldblox <raldblox@outlook.com>
2024-08-14 00:34:20 +0800

    add google analytics

--- dapp/app/layout.js
+++ dapp/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Providers } from "./providers/Providers";
 import { Montserrat } from "next/font/google";
 const montserrat = Montserrat({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   metadataBase: new URL("https://degeneratives.art"),
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={montserrat.className}>
         <Providers>
           <main className="text-black bg-white">{children}</main>
+          <Analytics />
         </Providers>
       </body>
     </html>
