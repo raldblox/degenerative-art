1d86a25ded7ac94a11ce18b652b399d6d0a50a2b
raldblox <raldblox@outlook.com>
2024-10-10 17:00:30 +0800

    update

--- frontend/components/sections/Feels.js
+++ frontend/components/sections/Feels.js
@@ -58,8 +58,8 @@ export default function Feels() {
   };
 
   return (
-    <div className="relative flex items-start justify-center w-full min-h-[calc(100vh-130px)] overflow-hidden ">
-      <div className="h-[calc(100vh-50px)] p-0 md:h-[calc(100vh-130px)] w-full flex justify-center items-start md:flex-row flex-col md:gap-3 md:px-6 overflow-hidden">
+    <div className="relative flex items-start justify-center w-full min-h-[calc(100vh-150px)] overflow-hidden ">
+      <div className="h-[calc(100vh-150px)] p-0 md:h-[calc(100vh-130px)] w-full flex justify-center items-start md:flex-row flex-col md:gap-3 md:px-6 overflow-hidden">
         <div className=" hidden md:grid md:max-w-[300px] min-h-[300px] relative content-between p-6 w-full col-span-1 bg-white text-foreground rounded-2xl backdrop-blur-sm">
           <div>
             <span>gm ser 🌤</span>
