aeba1477c7aee1235007072c9b459d4655b28996
raldblox <raldblox@outlook.com>
2024-10-10 12:39:29 +0800

    add suspense

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,6 +2,7 @@ import { Providers } from "@/providers/Providers";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Suspense>{children}</Suspense>
+        </Providers>
         <Analytics />
       </body>
     </html>
