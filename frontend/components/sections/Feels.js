8c70c4fa0c34a0765d1ef42e0138581daff4f7c4
raldblox <raldblox@outlook.com>
2024-10-12 16:35:21 +0800

    one loader

--- frontend/components/sections/Feels.js
+++ frontend/components/sections/Feels.js
@@ -226,41 +226,6 @@ export default function Feels() {
                     </CardFooter>
                   </Card>
                 </SwiperSlide>
-                <SwiperSlide className="bg-transparent animate-appearance-in">
-                  <Card className="w-full h-full bg-transparent light ">
-                    <CardHeader className="justify-between bg-white md:p-6">
-                      <div className="max-w-[300px] w-full flex items-center gap-3">
-                        <div>
-                          <Skeleton className="flex w-16 h-16 rounded-full" />
-                        </div>
-                        <div className="flex flex-col w-full gap-2">
-                          <Skeleton className="w-3/5 h-3 rounded-lg" />
-                          <Skeleton className="w-4/5 h-3 rounded-lg" />
-                        </div>
-                      </div>
-                    </CardHeader>
-                    <CardBody className="flex items-center justify-center w-full h-full">
-                      <div className="grid items-center content-center justify-center h-full grid-cols-3 gap-[1px] mx-auto w-fit">
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                        <Skeleton className="flex w-16 h-16 rounded-sm" />
-                      </div>
-                    </CardBody>
-                    <CardFooter className="items-center justify-between w-full h-16 gap-3 p-3 bg-white md:p-6">
-                      <div className="flex gap-1">
-                        <p className="font-semibold text-default-400 text-small">
-                          MOODART BY DEGENS
-                        </p>
-                      </div>
-                    </CardFooter>
-                  </Card>
-                </SwiperSlide>
               </>
             )}
           </Swiper>
