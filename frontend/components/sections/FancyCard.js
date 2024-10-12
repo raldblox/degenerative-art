02cebb60df31cbecbafea0fb3b4d97dd5201fc98
raldblox <raldblox@outlook.com>
2024-10-12 18:09:09 +0800

    upgraded: mojis

--- frontend/components/sections/FancyCard.js
+++ frontend/components/sections/FancyCard.js
@@ -67,7 +67,7 @@ export function CardPattern({ mouseX, mouseY, randomString }) {
         className="absolute inset-0 text-center duration-500 rounded-xl opacity-40 group-hover:animate-pulse group-hover/card:opacity-100 "
         style={style}
       >
-        <p className="absolute inset-x-0 h-full font-mono text-lg font-bold text-white break-words whitespace-pre-wrap transition-all duration-500">
+        <p className="absolute inset-x-0 h-full font-mono text-lg font-bold text-white break-words whitespace-pre-wrap transition-all duration-500 saturate-50">
           {randomString}
         </p>
       </motion.div>
@@ -76,13 +76,13 @@ export function CardPattern({ mouseX, mouseY, randomString }) {
 }
 
 const emojiRanges = [
-  [0x1f600, 0x1f64f], // Emoticons
+  // [0x1f600, 0x1f64f], // Emoticons
   // [0x1f300, 0x1f5ff], // Symbols & Pictographs
   //   [0x1f680, 0x1f6ff], // Transport & Map Symbols
   //   [0x2600, 0x26ff], // Miscellaneous Symbols
   //   [0x2700, 0x27bf], // Dingbats
-  //   [0x1f900, 0x1f9ff], // Supplemental Symbols and Pictographs
-  //   [0x1fa70, 0x1faff], // Symbols and Pictographs Extended-A
+  [0x1f900, 0x1f9ff], // Supplemental Symbols and Pictographs
+  [0x1fa70, 0x1faff], // Symbols and Pictographs Extended-A
 ];
 
 export const generateRandomString = (length) => {
